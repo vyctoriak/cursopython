aca6cf9b7b50340b4439933696cc17bbfdfe1d55
Vyctoria Karina <vyctoriak@gmail.com>
2019-03-26 22:29:29 -0300

    adicionado modificacoes

--- curso-python/adivinhacao.py
+++ curso-python/adivinhacao.py
@@ -1,15 +1,17 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
+numero_secreto = round(random.random() * 100)
+total_de_tentativas = 8
 rodada = 1
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
     chute_str = input("Digite um número entre 1 e 100: ")
-    print("Você digitou: ", chute_str)
+    print("Você digitou:", chute_str)
     chute = int(chute_str)
 
     if(chute < 1 or chute > 100):
