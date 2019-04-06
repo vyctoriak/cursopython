935254234789165ddf204ca45651846e4027c590
Vyctoria Karina <vyctoriak@gmail.com>
2019-04-06 18:24:38 -0300

    add forca game and final adjustments

--- curso-python/adivinhacao.py
+++ curso-python/adivinhacao.py
@@ -1,61 +1,59 @@
 import random
 
-print("*********************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("*********************************")
 
-numero_secreto = random.randrange(1, 101)
-# numero_secreto = 32
-total_de_tentativas = 0
-pontos = 1000
+def jogar():
 
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
 
-print("Qual o nível de dificuldade?")
-print("(1) Fácil (2) Médio (3) Difícil")
+    numero_secreto = random.randrange(1, 101)
+    total_de_tentativas = 0
+    pontos = 1000
 
-nivel = int(input("Defina o nível: "))
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
 
-if(nivel == 1):
-    total_de_tentativas = 20
-elif (nivel == 2):
-    total_de_tentativas = 10
-else:
-    total_de_tentativas = 5
+    nivel = int(input("Defina o nível: "))
 
-
-for rodada in range(1, total_de_tentativas + 1):
-    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute_str = input("Digite um número entre 1 e 100: ")
-    print("Você digitou:", chute_str)
-    chute = int(chute_str)
-
-    if(chute < 1 or chute > 100):
-        print("Você deve digitar um número entre 1 e 100!")
-        continue
-
-    acertou = chute == numero_secreto
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
-    pontos_perdidos = abs(numero_secreto - chute)
-    pontos = pontos - pontos_perdidos
-
-    if (acertou):
-        print("Você acertou!")
-        print('Você fez {} pontos!'.format(pontos))
-        break
+    if(nivel == 1):
+        total_de_tentativas = 20
+    elif (nivel == 2):
+        total_de_tentativas = 10
     else:
-        if(maior):
-            print("Você errou! O seu número foi maior que o número secreto.")
-            print("perdido:", pontos_perdidos)
-            # print("secreto:", numero_secreto)
-            print("resta:", pontos)
-        elif(menor):
-            print("Você errou! O seu número foi menor que o número secreto.")
-            print("perdido:", pontos_perdidos)
-            # print("secreto:", numero_secreto)
-            print("resta:", pontos)
-        rodada = rodada + 1
-        # pontos_perdidos = abs(numero_secreto - chute)
-        # pontos = pontos - pontos_perdidos
+        total_de_tentativas = 5
 
-print("Fim do jogo")
+    print('Você tem {} pontos!'.format(pontos))
+
+    for rodada in range(1, total_de_tentativas):
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+        chute_str = input("Digite um número entre 1 e 100: ")
+        print("Você digitou:", chute_str)
+        chute = int(chute_str)
+
+        if(chute < 1 or chute > 100):
+            print("Você deve digitar um número entre 1 e 100!")
+            continue
+
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
+
+        if (acertou):
+            print("Você acertou!")
+            print('Você fez {} pontos!'.format(pontos))
+            break
+        else:
+            if(maior):
+                print("Você errou! O seu número foi maior que o número secreto.")
+                # print("perdido:", pontos_perdidos)
+                # print("resta:", pontos)
+            elif(menor):
+                print("Você errou! O seu número foi menor que o número secreto.")
+                # print("perdido:", pontos_perdidos)
+                # print("resta:", pontos)
+            rodada = rodada + 1
+
+    print("Fim do jogo")
---
+++ curso-python/forca.py
@@ -0,0 +1,7 @@
+
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("*********************************")
+
+    print("Fim do jogo")
